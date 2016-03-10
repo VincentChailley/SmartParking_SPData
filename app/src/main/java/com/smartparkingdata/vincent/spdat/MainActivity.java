09d21c8ddfc1819f4390ff1c73d19073b701b3ad
VincentChailley <vchailley@gmail.com>
2016-03-10 21:35:12 +0100

    Google Maps implemented

--- app/src/main/java/com/smartparkingdata/vincent/spdat/MainActivity.java
+++ app/src/main/java/com/smartparkingdata/vincent/spdat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.smartparkingdata.vincent.spdat;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,43 +42,50 @@ public class MainActivity extends AppCompatActivity {
         Button btnGet2 = (Button)findViewById(R.id.btnGet2);
         tvData = (TextView)findViewById(R.id.tvJsonItem);
 
-        btnGet.setOnClickListener(new View.OnClickListener() {
+/*        btnGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(i==5) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest1.json");
-                    i=0;
-                }
-                if(i==4) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest2.json");
-                    i++;
-                }
                 if(i==3) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest1.json");
-                    i++;
+                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest3.json");
+                    i=0;
+                    return;
                 }
                 if(i==2) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest2.json");
+                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest4.json");
                     i++;
+                    return;
                 }
                 if(i==1) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest1.json");
+                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest5.json");
                     i++;
+                    return;
                 }
                 if(i==0) {
-                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest1.json");
+                    new JSONTask().execute("http://aureliemarcuard.com/sp/Jsontest6.json");
                     i++;
                 }
 
             }
         });
+*/
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+
+
 
         btnGet2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JSONTask().execute("http://www.aureliemarcuard.com/sp/Jsontest2.json");
+
+                new JSONTask().execute("http://www.aureliemarcuard.com/sp/Jsontest0.json");
             }
         });
+
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -134,14 +143,31 @@ public class MainActivity extends AppCompatActivity {
                     buffer.append(line);
                 }
 
-                return buffer.toString();
+                String finalJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONArray parentArray = parentObject.getJSONArray("estimations");
+
+                StringBuffer finalBufferedData = new StringBuffer();
+                for (int i=0; i<parentArray.length(); i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+
+                    String rue = finalObject.getString("rue");
+                    int estimation = finalObject.getInt("estimation");
+                    String longitude = (String) finalObject.get("longitude");
+                    String latitude = (String) finalObject.get("latitude");
+                    finalBufferedData.append(rue + " : " + estimation + longitude + ":" + latitude + "\n");
+                }
+
+                return finalBufferedData.toString();
+                //return buffer.toString();
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-//            } catch (JSONException e) {
-//                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             } finally {
                 if (connection != null) {
                     connection.disconnect();
@@ -163,4 +189,73 @@ public class MainActivity extends AppCompatActivity {
             tvData.setText(result);
         }
     }
+/*    public class JSONTaskold extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try{
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line="";
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                String finalJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONArray parentArray = parentObject.getJSONArray("estimations");
+
+                StringBuffer finalBufferedData = new StringBuffer();
+                for (int i=0; i<parentArray.length(); i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+
+                    String rue = finalObject.getString("rue");
+                    int estimation = finalObject.getInt("estimation");
+                    long longitude = (long) finalObject.get("longitude");
+                    long latitude = (long) finalObject.get("latitude");
+                    finalBufferedData.append(rue + " : " + estimation + "\n" + longitude + ":" + latitude + "\n");
+                }
+
+                return finalBufferedData.toString();
+                //return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
+    }*/
 }
